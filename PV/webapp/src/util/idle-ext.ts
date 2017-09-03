9fb46d36642dc25757bfee7aad1e3f7febaa9e4d
FateEduardo <a338694@uabc.edu.mx>
2017-09-03 13:02:17 -0700

    Update idle-ext.ts

--- PV/webapp/src/util/idle-ext.ts
+++ PV/webapp/src/util/idle-ext.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {Idle, SimpleExpiry} from "ng2-idle/core";
 
 /**
- * Created by Fernando Martinez on 26/1/16.
+ * 
  * Dependency for using idle (1.0.0-alpha.16) due existing issue with latest angular version (~2.0.1)
  * revisit once a non alpha version is released: https://github.com/HackedByChinese/ng2-idle/issues/17
  *
